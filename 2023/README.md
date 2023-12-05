b891f03effb100e6f834610f0311fdf687d824fe
Andrew Fricke <160705+uhnomoli@users.noreply.github.com>
2023-12-05 02:30:40 -0500

    Add solution for 2023 day 1

---
+++ 2023/README.md
@@ -0,0 +1,30 @@
+# advent-of-code-2023
+
+These solutions depend on [Bun][bun]. Use your desired method of installation,
+after which, to install the required dependencies run:
+
+```shell
+$ bun install
+```
+
+Usage:
+
+```shell
+$ bun solve.ts -h
+Usage: advent-of-code-2023 [options]
+
+Solutions to challenges from Advent of Code 2023
+
+Options:
+  -p, --part <number>  part to solve (default: 1)
+  -d, --day <number>   day to solve
+  -i, --input <path>   path to challenge input
+  -V, --version        output the version number
+  -h, --help           display help for command
+$ bun solve.ts -d 1 -p 2 -i ./inputs/1.txt
+[.] Solution: <REDACTED>
+```
+
+
+[bun]: https://bun.sh/
+
---
+++ 2023/solutions/1.ts
@@ -0,0 +1,68 @@
+function first(data: string): void {
+    let result = 0;
+    data.split('\n').forEach(line => {
+        let first = 0;
+        let last = 0;
+
+        for (let i = 0; i < line.length; ++i) {
+            const o = line.charCodeAt(i);
+            if (o > 0x2f && o < 0x3a) {
+                last = o - 0x30;
+                if (first === 0) {
+                    first = last * 10;
+                }
+            }
+        }
+
+        result += first + last;
+    });
+
+    console.log(`[.] Solution: ${result}`);
+}
+
+function second(data: string): void {
+    const pattern = /(?:[0-9]|one|two|three|four|five|six|seven|eight|nine)/g;
+    let result = 0;
+    const to_digit: Record<string, number> = {
+        '0': 0, 'zero': 0,
+        '1': 1, 'one': 1,
+        '2': 2, 'two': 2,
+        '3': 3, 'three': 3,
+        '4': 4, 'four': 4,
+        '5': 5, 'five': 5,
+        '6': 6, 'six': 6,
+        '7': 7, 'seven': 7,
+        '8': 8, 'eight': 8,
+        '9': 9, 'nine': 9};
+
+    data.split('\n').forEach(line => {
+        let first = 0;
+        let last = 0;
+        let match = null;
+
+        while ((match = pattern.exec(line)) !== null) {
+            last = to_digit[match[0]];
+            if (first === 0) {
+                first = last * 10;
+            }
+
+            pattern.lastIndex = match.index + Math.max(match[0].length - 1, 1);
+        }
+
+        result += first + last;
+    });
+
+    console.log(`[.] Solution: ${result}`);
+}
+
+
+export function run(part: number, input: string): void {
+    Bun.file(input).text().then(data => {
+        if (part === 1) {
+            first(data);
+        } else {
+            second(data);
+        }
+    });
+}
+
