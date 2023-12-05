ab4dc415a589162fc42bfe9703b4cf63ca89a519
Andrew Fricke <uhnomoli@users.noreply.github.com>
2023-12-05 12:48:36 -0500

    Change forEach to for/of

--- 2023/solutions/2.ts
+++ 2023/solutions/2.ts
@@ -1,43 +1,50 @@
+function game_validate(input: string): boolean {
+    for (const match of input.matchAll(/(\d+) (blue|green|red)/g)) {
+        const n = parseInt(match[1], 10);
+        const color = match[2];
+
+        if (color === 'blue' && n > 14) { return false; }
+        if (color === 'green' && n > 13) { return false; }
+        if (color === 'red' && n > 12) { return false; }
+    }
+
+    return true;
+}
+
+
 function first(data: string): void {
     let result = 0;
-    data.split('\n').forEach(line => {
+    for (const line of data.split('\n')) {
         const [game, input] = line.split(': ', 2);
 
-        for (const match of input.matchAll(/(\d+) (blue|green|red)/g)) {
-            const n = parseInt(match[1], 10);
-            const color = match[2];
-
-            if (color === 'blue' && n > 14) { return; }
-            if (color === 'green' && n > 13) { return; }
-            if (color === 'red' && n > 12) { return; }
+        if (game_validate(input)) {
+            result += parseInt(game.split(' ')[1], 10);
         }
-
-        result += parseInt(game.split(' ')[1], 10);
-    });
+    }
 
     console.log(`[.] Solution: ${result}`);
 }
 
 function second(data: string): void {
     let result = 0;
-    data.split('\n').forEach(line => {
+    for (const line of data.split('\n')) {
         const [game, input] = line.split(': ', 2);
         const dice: Record<string, number> = {
             'blue': 0,
             'green': 0,
             'red': 0};
 
-        input.split('; ').forEach(pull => {
+        for (const pull of input.split('; ')) {
             for (const match of pull.matchAll(/(\d+) (blue|green|red)/g)) {
                 const n = parseInt(match[1], 10);
                 const color = match[2];
 
                 dice[color] = Math.max(dice[color], n);
             }
-        });
+        }
 
         result += dice['blue'] * dice['green'] * dice['red'];
-    });
+    }
 
     console.log(`[.] Solution: ${result}`);
 }
