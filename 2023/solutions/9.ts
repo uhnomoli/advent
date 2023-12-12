346b683d6fb64e13da47dc2086b7a53e3d671583
Andrew Fricke <160705+uhnomoli@users.noreply.github.com>
2023-12-12 02:39:52 -0500

    Add solution for 2023 day 9

---
+++ 2023/solutions/9.ts
@@ -0,0 +1,95 @@
+interface History {
+    sequences: number[][];
+}
+
+
+function history_next(history: History): number {
+    const sequences = history.sequences;
+    let value = sequences[sequences.length - 2][0];
+
+    for (let i = sequences.length - 3; i >= 0; i--) {
+        const sequence = sequences[i];
+
+        value += sequence[sequence.length - 1];
+    }
+
+    return value;
+}
+
+function history_parse(data: string): History {
+    const history: History = {
+        sequences: [sequence_parse(data)]};
+
+    for (const sequence of history.sequences) {
+        const values: number[] = [];
+
+        for (let i = 1; i < sequence.length; i++) {
+            values.push(sequence[i] - sequence[i - 1]);
+        }
+
+        history.sequences.push(values);
+
+        const sum = values
+            .reduce((accumulator, value, index) => accumulator + value);
+        if (sum === 0) { break; }
+    }
+
+    return history;
+}
+
+function history_previous(history: History): number {
+    const sequences = history.sequences;
+    let value = sequences[sequences.length - 2][0];
+
+    for (let i = sequences.length - 3; i >= 0; i--) {
+        const sequence = sequences[i];
+
+        value = sequence[0] - value;
+    }
+
+    return value;
+}
+
+function sequence_parse(data: string): number[] {
+    return data.split(' ')
+        .map(number => parseInt(number, 10));
+}
+
+
+function first(data: string): void {
+    let result = 0;
+
+    const histories: History[] = data.split('\n')
+        .map(line => history_parse(line));
+    for (const history of histories) {
+        result += history_next(history);
+    }
+
+    console.log(`[.] Solution: ${result}`);
+}
+
+function second(data: string): void {
+    let result = 0;
+
+    const histories: History[] = data.split('\n')
+        .map(line => history_parse(line));
+    for (const history of histories) {
+        result += history_previous(history);
+    }
+
+    console.log(`[.] Solution: ${result}`);
+}
+
+
+export function run(part: number, input: string): void {
+    Bun.file(input).text().then(data => {
+        data = data.trim();
+
+        if (part === 1) {
+            first(data);
+        } else {
+            second(data);
+        }
+    });
+}
+
