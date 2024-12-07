d2e9c1ac83f330142278116c3427a329177de7b7
Andrew Fricke <160705+uhnomoli@users.noreply.github.com>
2024-12-07 00:02:09 -0500

    Update solution to 2024 day 4 part 1

--- 2024/solutions/4.ts
+++ 2024/solutions/4.ts
@@ -22,61 +22,64 @@ interface Grid {
 }
 
 
-function neighbor_get(grid: Grid, cell: Cell, direction: Direction): Cell {
+function neighbor_get(
+        grid: Grid, cell: Cell, direction: Direction): Cell | null {
     switch (direction) {
         case Direction.N:
+            if (cell.y === 0) { break; }
+
             return grid.cells[cell.y - 1][cell.x];
         case Direction.NE:
+            if (cell.x === grid.width || cell.y === 0) { break; }
+
             return grid.cells[cell.y - 1][cell.x + 1];
         case Direction.E:
+            if (cell.x === grid.width) { break; }
+
             return grid.cells[cell.y][cell.x + 1];
         case Direction.SE:
+            if (cell.x === grid.width || cell.y === grid.height) { break; }
+
             return grid.cells[cell.y + 1][cell.x + 1];
         case Direction.S:
+            if (cell.y === grid.height) { break; }
+
             return grid.cells[cell.y + 1][cell.x];
         case Direction.SW:
+            if (cell.x === 0 || cell.y === grid.height) { break; }
+
             return grid.cells[cell.y + 1][cell.x - 1];
         case Direction.W:
+            if (cell.x === 0) { break; }
+
             return grid.cells[cell.y][cell.x - 1];
         case Direction.NW:
+            if (cell.x === 0 || cell.y === 0) { break; }
+
             return grid.cells[cell.y - 1][cell.x - 1];
         default:
             throw new Error('Invalid grid coordinate');
     }
+
+    return null;
 }
 
-function neighbors_get(
-        grid: Grid, cell: Cell, direction: Direction | null): Map<Direction, Cell> {
-    const directions: Map<Direction, boolean> = new Map([
-         [Direction.N, cell.y !== 0],
-        [Direction.NE, cell.x !== grid.width && cell.y !== 0],
-         [Direction.E, cell.x !== grid.width],
-        [Direction.SE, cell.x !== grid.width && cell.y !== grid.height],
-         [Direction.S, cell.y !== grid.height],
-        [Direction.SW, cell.x !== 0 && cell.y !== grid.height],
-         [Direction.W, cell.x !== 0],
-        [Direction.NW, cell.x !== 0 && cell.y !== 0]]);
+function neighbor_search(
+        grid: Grid, cell: Cell, direction: Direction, word: string): boolean {
+    if (cell.value !== word[0]) { return false; }
+    if (word.length === 1) { return true; }
 
-    const neighbors: Map<Direction, Cell> = new Map();
-    if (direction === null) {
-        for (const [key, valid] of directions) {
-            if (valid === true) {
-                neighbors.set(key, neighbor_get(grid, cell, key));
-            }
-        }
-    } else if (directions.get(direction) === true) {
-        neighbors.set(direction, neighbor_get(grid, cell, direction));
-    }
+    const neighbor = neighbor_get(grid, cell, direction);
+    if (neighbor === null) { return false; }
 
-    return neighbors;
+    return neighbor_search(grid, neighbor, direction, word.slice(1));
 }
 
-function neighbors_search(grid: Grid, cell: Cell, word: string): number {
+function word_search(grid: Grid, start: Cell, word:string): number {
     let matches = 0;
 
-    const neighbors = neighbors_get(grid, cell, null);
-    for (const [direction, neighbor] of neighbors) {
-        if (word_search(grid, neighbor, direction, word)) {
+    for (const direction of Object.values(Direction)) {
+        if (neighbor_search(grid, start, direction, word)) {
             ++matches;
         }
     }
@@ -84,35 +87,46 @@ function neighbors_search(grid: Grid, cell: Cell, word: string): number {
     return matches;
 }
 
-function word_search(
-        grid: Grid, cell: Cell, direction: Direction, word: string): boolean {
-    if (cell.value !== word[0]) { return false; }
-    if (word.length === 1) { return true; }
 
-    const neighbors = neighbors_get(grid, cell, direction);
-
-    const neighbor = neighbors.get(direction);
-    if (neighbor === undefined) { return false; }
-
-    return word_search(grid, neighbor, direction, word.slice(1));
-}
-
-
-function first(grid: Grid): void {
+function first(data: string): void {
     let result = 0;
 
-    for (const row of grid.cells) {
-        for (const cell of row) {
-            if (cell.value === 'X') {
-                result += neighbors_search(grid, cell, 'MAS');
+    const grid: Grid = {
+        cells: [],
+        height: 0,
+        width: 0};
+    const starts: Cell[] = [];
+
+    for (const [y, line] of data.split('\n').entries()) {
+        const row: Cell[] = [];
+
+        for (const [x, character] of line.split('').entries()) {
+            const cell: Cell = {
+                value: character,
+                x: x,
+                y: y};
+
+            if (character === 'X') {
+                starts.push(cell);
             }
+
+            row.push(cell);
         }
+
+        grid.cells.push(row);
+    }
+
+    grid.height = grid.cells.length - 1;
+    grid.width = grid.cells[0].length - 1;
+
+    for (const start of starts) {
+        result += word_search(grid, start, 'XMAS');
     }
 
     console.log(`[.] Solution: ${result}`);
 }
 
-function second(grid: Grid): void {
+function second(data: string): void {
     let result = 0;
 
     console.log(`[.] Solution: ${result}`);
@@ -123,22 +137,10 @@ export function run(part: number, input: string): void {
     Bun.file(input).text().then(data => {
         data = data.trim();
 
-        const grid: Grid = {
-            cells: [],
-            height: 0,
-            width: 0};
-        for (const [y, line] of data.split('\n').entries()) {
-            grid.cells.push(line.split('')
-                .map((character, x) => ({value: character, x: x, y: y})));
-        }
-
-        grid.height = grid.cells.length - 1;
-        grid.width = grid.cells[0].length - 1;
-
         if (part === 1) {
-            first(grid);
+            first(data);
         } else {
-            second(grid);
+            second(data);
         }
     });
 }
